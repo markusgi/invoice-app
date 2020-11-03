e8b6e7a48feded56202bb16953a3e63736854161
Markus Giger <markusgig@gmail.com>
2020-11-03 14:39:59 +0100

    backend users model and nginx config

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView, RetrieveAPIView
 
-# Create your views here.
+from users.serializers import UserSerializer
+
+User = get_user_model()
+
+
+#list all users: api/users/list/
+class ListUsersAPIView(ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = []
+
+
+#list patch logged in user: api/users/me/
+class RetrieveUpdateMeAPIView(RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def get_object(self):
+        return self.request.user
+
+#Retrieve certain user: api/users/id
+class RetrieveCertainUserAPIView(RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = []
+    lookup_field = 'id'
