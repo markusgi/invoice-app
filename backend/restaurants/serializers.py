ad31bfd7337c0aad443debf4838ecea8ab693d15
lisc119 <mjchoo119@gmail.com>
2020-11-05 15:38:16 +0100

    modify the serializers of restaurants

--- backend/restaurants/serializers.py
+++ backend/restaurants/serializers.py
@@ -1,16 +1,31 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from users.serializers import SmallUserSerializer
 from restaurants.models import Restaurant
+from users.serializers import SmallUserSerializer
+from invoices.serializers import InvoiceSerializer
+from revenues.serializers import RevenueSerializer
 
 User = get_user_model()
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
     author = SmallUserSerializer(required=False, read_only=True)
+    revenues = RevenueSerializer(required=False, read_only=True, many=True)
+    invoices = InvoiceSerializer(required=False, read_only=True, many=True)
 
     class Meta:
         model = Restaurant
-        fields = '__all__'
+        fields = ['id', 'author', 'created', 'updated', 'name', 'country', 'street', 'city', 'zip', 'website', 'phone',
+                  'email', 'image', 'revenues', 'invoices']
+        read_only_fields = ['author']
+
+
+class SmallRestaurantSerializer(serializers.ModelSerializer):
+    author = SmallUserSerializer(required=False, read_only=True)
+
+    class Meta:
+        model = Restaurant
+        fields = ['id', 'author', 'created', 'updated', 'name', 'country', 'street', 'city', 'zip', 'website', 'phone',
+                  'email', 'image']
         read_only_fields = ['author']
