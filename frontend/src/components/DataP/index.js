11d229abf399e8e02da4ef532a82ea5098a7c109
Markus Giger <markusgig@gmail.com>
2020-11-09 11:43:47 +0100

    added inputs viren

--- frontend/src/components/DataP/index.js
+++ frontend/src/components/DataP/index.js
@@ -2,7 +2,11 @@ import React, { Fragment } from 'react';
 
 import { NiceInput, InvoiceForm } from '../../style/Form';
 
+import { Password, EmailAddress, FormGroup, Label2, Input2 } from '../../style/Inputs';
+
 const DataPage = () => {
+
+    let emailOnChangeHandler;
 
 
     return (
@@ -36,6 +40,16 @@ const DataPage = () => {
                 <NiceInput type="text" name="tag" id="tag" />
                 
             </form>
+
+            <Password type="password" placeholder="Password"></Password>
+            <Input2 type="text" placeholder="Last Name" />
+            <FormGroup>
+                <Label2>First Name</Label2>
+                <Input2 type="text" />
+            </FormGroup>
+
+            <EmailAddress type="text" placeholder="Email Address" onChange={emailOnChangeHandler}/>
+
 
         </Fragment>
     );
