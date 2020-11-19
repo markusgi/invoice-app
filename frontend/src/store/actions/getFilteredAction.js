19e32af9deb84b9ef8440c447afa3c7236a206ef
Markus Giger <markusgig@gmail.com>
2020-11-19 23:13:34 +0100

    changed url all tags

--- frontend/src/store/actions/getFilteredAction.js
+++ frontend/src/store/actions/getFilteredAction.js
@@ -1,7 +1,7 @@
 import baseUrl from '../../helper/url_helper';
 
 export const getFilteredAction = ( start, end, tag, token, search ) => async (dispatch, getState) => {
-	const url = `${baseUrl}articles/date/${start}/${end}/tag/${tag}`;
+	const url = `${baseUrl}articles/date/${start}/${end}/`;
 	const config = {
 		method: 'GET',
 		headers: new Headers({
